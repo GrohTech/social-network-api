1c0da9f62738daeed56f9c2ba47e4a7ad2a7af52
Lauren Groh <legroh@uwm.edu>
2021-12-28 19:27:24 -0600

    map out thought and user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,6 +3,25 @@ const router = require('express').Router();
 
 // Import User routes
 
+// /api/users
+// GET all users
+
+// GET a single user by _id and populated thought and friend data
+
+// POST a new user
+
+// PUT to update a user by its _id
+
+// DELETE to remove user by its _id
+
+// BONUS remove a user's associated thoughts when deleted
+
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
 
 // Export router
 module.exports = router;
